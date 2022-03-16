ab3cdfcdf93d7a75ad37544c92708a9e60a923fd
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-16 19:55:46 +0700

    define signin & protected page

---
+++ pages/protected.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+
+const ProtectedPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Protected Page</h1>
+        </div>
+    )
+}
+
+export default ProtectedPage
---
+++ pages/signin.tsx
@@ -0,0 +1,13 @@
+
+import { NextPage } from "next";
+
+
+const SignInPage: NextPage = () => {
+    return (
+        <div>
+            <h1>Sign in</h1>
+        </div>
+    )
+}
+
+export default SignInPage
