1fd55751e90c8d5e40b0638d973d29f8f2b261af
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-16 20:18:04 +0700

    setup session for protected route

--- pages/protected.tsx
+++ pages/protected.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from "next";
-
+import { useSession } from "next-auth/react"
 
 const ProtectedPage: NextPage = () => {
+
+    const { data: session, status } = useSession()
+
+    if (!session) {
+        return (
+            <div>
+                <p>Access Denied</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>Protected Page</h1>
