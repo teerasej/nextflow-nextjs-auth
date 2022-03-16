7c72d7eb04bb0768316dfd0be860f20deb422188
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-16 20:04:37 +0700

    define form data and submit to api

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,13 +1,42 @@
 
 import { NextPage } from "next";
+import { FormEvent } from "react";
 
+type LoginFormElements = {
+    username: HTMLInputElement,
+    password: HTMLInputElement
+} & HTMLCollection
 
 const SignInPage: NextPage = () => {
-    return (
-        <div>
-            <h1>Sign in</h1>
-        </div>
-    )
+
+    const goSignIn = async (event:FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+        const loginForm =  (event.currentTarget as HTMLFormElement).elements as LoginFormElements
+
+        const response = await fetch('/api/auth', {
+          body: JSON.stringify({
+            username: loginForm.username.value,
+            password: loginForm.password.value
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST'
+        })
+    
+        const result = await response.json()
+        console.log(result.message)
+      }
+      return (
+        <form onSubmit={goSignIn}>
+          <label data-for="username">username:</label>
+          <input type="text" id="username" name="username" />
+          <label data-for="password">password:</label>
+          <input type="password" id="password" name="password" />
+          <button type="submit">Sign in</button>
+        </form>
+      )
 }
 
 export default SignInPage
