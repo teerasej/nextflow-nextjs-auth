6cc870202438d33cd5754e6dd3573954cfa90cf3
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-03-16 22:59:49 +0700

    use signin function from next-auth

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,8 @@
 
 import { NextPage } from "next";
+import { RedirectableProviderType } from "next-auth/providers";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import { FormEvent } from "react";
 
 type LoginFormElements = {
@@ -7,26 +10,35 @@ type LoginFormElements = {
     password: HTMLInputElement
 } & HTMLCollection
 
+type AuthResponse = {
+  error: string | undefined,
+  status: number,
+  ok: boolean,
+  url: string | null
+} & RedirectableProviderType
+
 const SignInPage: NextPage = () => {
+
+    const router = useRouter()
 
     const goSignIn = async (event:FormEvent<HTMLFormElement>) => {
 
         event.preventDefault();
         const loginForm =  (event.currentTarget as HTMLFormElement).elements as LoginFormElements
 
-        const response = await fetch('/api/auth', {
-          body: JSON.stringify({
-            username: loginForm.username.value,
-            password: loginForm.password.value
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          method: 'POST'
+        const res = await signIn<AuthResponse>('credentials', {
+          username: loginForm.username.value,
+          password: loginForm.password.value,
+          redirect: false
         })
-    
-        const result = await response.json()
-        console.log(result.message)
+
+        if (res?.error) {
+          return <div><p>Error: ${res?.error}</p></div>
+        }
+        if (res?.url) {
+          router.push(`/protected`)
+        }
+
       }
       return (
         <form onSubmit={goSignIn}>
